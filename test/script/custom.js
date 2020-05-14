6111e83c4a75b990ee7cf7e39e07a8a786d964cf
sdw6379 <65323135+sdw6379@users.noreply.github.com>
2020-05-14 19:00:08 +0900

    2020.05.14

---
+++ test/script/custom.js
@@ -0,0 +1,21 @@
+var mySwiper = new Swiper ('#banner .swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:'true',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+      }
+  });
+
+var mySwiper2 = new Swiper('.notice .swiper-container',{
+  loop: true,
+  spaceBetween: 60,
+  slidesPerView: 'auto',
+  centeredSlides: true
+}); 
